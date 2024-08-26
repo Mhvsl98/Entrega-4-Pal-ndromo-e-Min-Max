9b279265671534d5244487e0c736ddb80b11f3b2
Mhvsl98 <m4vslofc@gmail.com>
2024-08-26 09:18:06 -0300

    Committe Min e Max

--- script.js
+++ script.js
@@ -12,5 +12,25 @@ function isPalindrome(str) {
 console.log(isPalindrome("A torre da derrota"));
 
 function arrayMaxMin(arr) {
-  /* Seu código aqui */
+  // Inicia as variáveis auxiliares com o valor 0, conforme a dica.
+  let minValue = arr[0];
+  let maxValue = arr[0];
+
+  // Laço de repetição para percorrer o array
+  for (let i = 0; i < arr.length; i++) {
+    // Se o valor atual é menor que o valor mínimo encontrado até agora, atualize minValue
+    if (arr[i] <= minValue) {
+      minValue = arr[i];
+    }
+    // Se o valor atual é maior que o valor máximo encontrado até agora, atualize maxValue
+    if (arr[i] >= maxValue) {
+      maxValue = arr[i];
+    }
+  }
+
+  // Retorna um novo array com o valor mínimo e máximo
+  return [minValue, maxValue];
 }
+console.log(arrayMaxMin(array));
+
+//let array = [1, 52, 59, 16, 13, 9];
