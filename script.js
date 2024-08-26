bd797332d6f3d1474976af1d9d7635e898d638f4
Mhvsl98 <m4vslofc@gmail.com>
2024-08-26 08:36:17 -0300

    Committe Palindrome

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  let contrario = "";
+  str = str.toLocaleLowerCase().replace(/[^a-z0-9]/g, "");
+  for (let i = str.length - 1; i >= 0; i--) {
+    contrario += str[i];
+    if (contrario === str) {
+      return true;
+    }
+  }
+  return false;
 }
+console.log(isPalindrome("A torre da derrota"));
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  /* Seu código aqui */
 }
